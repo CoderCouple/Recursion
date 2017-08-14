f53c42a2fe8908ee357d369109fe3dbc8089d5d2
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-14 14:25:28 -0700

    Create 6 AllPossibleCombinationsOf_2and3_DigitOnly.java

---
+++ 6 AllPossibleCombinationsOf_2and3_DigitOnly.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class AllPossibleCombinationsOf_2and3_DigitOnly {
+	
+	public static void getAllPossibleCombinationsOf_2and3_DigitOnly(int array[], int index, String s, int digits) {
+		if (index >= array.length) {
+			if (2 <= digits && digits <= 3) {
+				System.out.println(s);
+				return;
+			}
+
+			return;
+		}
+
+		getAllPossibleCombinationsOf_2and3_DigitOnly(array, index + 1, s + array[index], digits + 1);
+		getAllPossibleCombinationsOf_2and3_DigitOnly(array, index + 1, s, digits);
+
+	}
+
+	public static void main(String[] args) {
+		int array[] = { 1, 2, 3, 4, 5 };
+		getAllPossibleCombinationsOf_2and3_DigitOnly(array, 0, "", 0);
+	}
+
+}
